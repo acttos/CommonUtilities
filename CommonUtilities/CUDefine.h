999d2352a837aad5afeadcbcbbb44f057cc150b1
acttos <majinshou@gmail.com>
2016-11-21 14:26:53 +0800

    Change the style of Logger in model 'DEBUG'.

--- CommonUtilities/CUDefine.h
+++ CommonUtilities/CUDefine.h
@@ -7,7 +7,8 @@
 //
 
 #ifdef DEBUG
-#define Logger(format, ...) NSLog(format, ## __VA_ARGS__)
+//#define Logger(format, ...) NSLog(format, ## __VA_ARGS__)
+#define Logger(...) NSLog(@"%s:%d %@", __PRETTY_FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__])
 #else
 #define Logger(format, ...)
 #endif
