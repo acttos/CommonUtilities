65011168f9894ead78c0e422bbc2b9bd4dcae21d
acttos <majinshou@gmail.com>
2016-11-23 17:58:57 +0800

    Add comments for CULabel.h.

--- CommonUtilities/CUView/CULabel.h
+++ CommonUtilities/CUView/CULabel.h
@@ -11,6 +11,13 @@
 
 @interface CULabel : NSObject
 
+/**
+ 计算一个UILabel视图在父视图中的大小和位置，UILabel的高度和宽度在实际布局中，还是很有实际意义的。
+
+ @param label 要用于计算的UILabel实例
+ @param view UILabel实例的父视图
+ @return UILabel在父视图中的大小和位置参数
+ */
 +(CGSize)sizeOfLabel:(UILabel *)label inView:(UIView *)view;
 
 @end
