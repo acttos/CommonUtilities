626bb8a8bab00d0827f61f341567efcaeb87474b
majinshou <majinshou@gmail.com>
2016-11-25 19:18:49 +0800

    Add test case for CommonUtilities.

---
+++ CommonUtilities-Test/CommonUtilities_Test.m
@@ -0,0 +1,52 @@
+//
+//  CommonUtilities_Test.m
+//  CommonUtilities-Test
+//
+//  Created by Acttos on 25/11/2016.
+//  Copyright © 2016 Acttos.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "CommonUtilities.h"
+
+@interface CommonUtilities_Test : XCTestCase
+
+@end
+
+@implementation CommonUtilities_Test
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    // 初始化的代码，在测试方法调用之前调用
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // 释放测试用例的资源代码，这个方法会每个测试用例执行后调用
+    [super tearDown];
+}
+
+- (void)testCUCode {
+    NSString *identifier = [CUCode uniqueIdentifier];
+    
+    XCTAssertNotNil(identifier);
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // 测试用例的例子，注意测试用例一定要test开头
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    // 测试性能例子
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+        // 需要测试性能的代码
+    }];
+}
+
+@end
