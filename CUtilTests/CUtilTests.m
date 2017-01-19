84a1e45bbc465d193d8f1db0ca75fe4e709a3f80
majinshou <majinshou@gmail.com>
2017-01-19 14:32:08 +0800

    Remove some Chinese.

--- CUtilTests/CUtilTests.m
+++ CUtilTests/CUtilTests.m
@@ -20,12 +20,10 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    // 初始化的代码，在测试方法调用之前调用
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-    // 释放测试用例的资源代码，这个方法会每个测试用例执行后调用
     [super tearDown];
 }
 
